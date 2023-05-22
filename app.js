204d3b4f485e24d7996cd48de39ad22d5525eff1
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-22 22:47:52 +0530

    tours get request

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  //   res
-  //     .status(200)
-  //     .json({ msg: 'this is a get request', type: 'this is json!!' });
-  res.status(404).json('I dont have that');
-});
+// app.get('/', (req, res) => {
+//   //   res
+//   //     .status(200)
+//   //     .json({ msg: 'this is a get request', type: 'this is json!!' });
+//   res.status(404).json('I dont have that');
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('Post method');
+// app.post('/', (req, res) => {
+//   res.status(200).send('Post method');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
