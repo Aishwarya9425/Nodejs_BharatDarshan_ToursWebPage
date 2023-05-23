2b8643057b56f16a07986e8ce6f34e8bcde84a79
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-23 16:27:34 +0530

    post request

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   //   res
-//   //     .status(200)
-//   //     .json({ msg: 'this is a get request', type: 'this is json!!' });
-//   res.status(404).json('I dont have that');
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).send('Post method');
-// });
+//middleware -- stands b/w req and response
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -26,6 +18,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //now to temp save in tours json create id, else mongo db will take care of id automatically
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  console.log('newTour', newTour);
+  tours.push(newTour);
+  //push into file
+  //we are inside callback fun already and we cant block the event loop
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      //201 req created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
