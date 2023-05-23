149f6ed6470912821d46b0b6a8ab2c99e7a1d004
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-23 17:20:44 +0530

    added get specific tour by id, patch

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 //middleware -- stands b/w req and response
 app.use(express.json());
 
+//get the updated tours json from the file -- will later use mongo db
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -20,6 +22,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+//get specific tour by id
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params); //all params
+  //req.param originally string
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({ status: 'Failed', message: 'Invalid id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour, //get the specific tour by id
+    },
+  });
+});
+
+//create a new tour
 app.post('/api/v1/tours', (req, res) => {
   //now to temp save in tours json create id, else mongo db will take care of id automatically
   const newId = tours[tours.length - 1].id + 1;
@@ -41,6 +62,18 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id > tours.length) {
+    return res.status(404).json({ status: 'Failed', message: 'Invalid id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour.... ',
+    },
+  });
 });
 
 const port = 3000;
