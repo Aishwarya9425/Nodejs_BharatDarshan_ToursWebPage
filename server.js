9e230a3e9a7857eff0f531e1f1f92807ce9a83ce
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-25 22:59:14 +0530

    mongoose schema, model

--- server.js
+++ server.js
@@ -11,15 +11,49 @@ const DB = process.env.DATABASE.replace(
 );
 //connect to mongo, it returns a promise ;
 mongoose
- //.connect(process.env.DATABASE_LOCAL, { //connect to local db
-  .connect(DB, { //connect to cluster
+  //.connect(process.env.DATABASE_LOCAL, { //connect to local db
+  .connect(DB, {
+    //connect to cluster
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
   .then((con) => {
-   // console.log(con.connections);
+    // console.log(con.connections);
     console.log('Connected to mongo db successfully!!');
+  });
+
+//schema with validations, required etc
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 3,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a name'],
+  },
+});
+
+//model created from schema
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'test111',
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR', err);
   });
 
 console.log(app.get('env')); //development or production
