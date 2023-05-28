ac1fd262c8b4ae7a33824d2fa9c9be4fff26476e
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-28 21:51:41 +0530

    refactoring for mvc

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
 //get the updated tours json from the file -- will later use mongo db
-const tours = JSON.parse(
+/* const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+); */
 
-//check id - use middleware
-exports.checkID = (req, res, next, value) => {
+//check id - use middleware -- not needed because mongoose will generate unique id
+/* exports.checkID = (req, res, next, value) => {
   console.log(`---Param Middleware checkID running--`);
   console.log(`Tour id is: ${value}`);
   if (req.params.id > tours.length) {
     return res.status(404).json({ status: 'Failed', message: 'Invalid id' });
   }
   next();
-};
+}; */
 
 //checkBody -  middleware
 //use req.param for param md, otherwise req.body -- post req,
@@ -31,11 +31,11 @@ exports.checkBody = (req, res, next) => {
 exports.getAlltours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    requestedTime: req.requestedTime,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // requestedTime: req.requestedTime,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -43,37 +43,37 @@ exports.getTour = (req, res) => {
   console.log(req.params); //all params
   //req.param originally string
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour, //get the specific tour by id
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour, //get the specific tour by id
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
   //now to temp save in tours json create id, else mongo db will take care of id automatically
   console.log(`Creating new tour with data...`);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  console.log('newTour', newTour);
-  tours.push(newTour);
-  //push into file
-  //we are inside callback fun already and we cant block the event loop
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      //201 req created
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // console.log('newTour', newTour);
+  // tours.push(newTour);
+  // //push into file
+  // //we are inside callback fun already and we cant block the event loop
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     //201 req created
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: {
+  //         tour: newTour,
+  //       },
+  //     });
+  //   }
+  // );
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -23,39 +23,6 @@ mongoose
     console.log('Connected to mongo db successfully!!');
   });
 
-//schema with validations, required etc
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 3,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a name'],
-  },
-});
-
-//model created from schema
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'test111',
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR', err);
-  });
-
 console.log(app.get('env')); //development or production
 //console.log(process.env);
 const port = process.env.PORT || 3000;
