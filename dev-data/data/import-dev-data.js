93d6e7adb287de46de909cc62c6a106ab0d940a6
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-05-29 17:36:25 +0530

    import data to db using process.argv cli

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' }); //1st read the env file
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to mongo db successfully!!');
+  });
+
+//read tourjson file, convert json into obj
+const tourObj = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//import data into db
+const importData = async () => {
+  try {
+    await Tour.create(tourObj);
+    console.log('Tour data created successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//delete all data from db
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); //delet all documents
+    console.log('Deleted all documents!!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//via cli command, depending on what you specify
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a Group Size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 3,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image Cover'],
+  },
+  images: [String], //arr of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates : [Date] // arr of dates
 });
 
 //model created from schema
