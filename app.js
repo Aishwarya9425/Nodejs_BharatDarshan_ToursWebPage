5111d5e7b0231b7010f1adbaad79fab7535447a6
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-06-03 14:17:20 +0530

    ndb node debugger

--- app.js
+++ app.js
@@ -34,5 +34,12 @@ app.use((req, res, next) => {
 //middleware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+//middleware runs according to the order defined
+//middleware to handle all undefined routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 module.exports = app;
