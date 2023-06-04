92eae26344bb64d1d003fc38a101eade7041737a
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-06-04 16:03:17 +0530

    unhandledRejection uncaughtException

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,6 +15,8 @@ const handleDuplicateFieldsDB = (err) => {
 };
 
 const handleValidationErrorDB = (err) => {
+  //could have many validation errors
+  //get all the messages
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
--- server.js
+++ server.js
@@ -2,6 +2,14 @@
 //since this is the starting file, need to run from here not app.js
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+//should be at the top so it catches all uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Server Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1); //1 for uncaught exception
+});
+
 dotenv.config({ path: './config.env' }); //1st read the env file
 const app = require('./app'); //then require app file
 
@@ -27,6 +35,17 @@ mongoose
 console.log(app.get('env')); //development or production
 //console.log(process.env);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
+});
+
+//unhandled promise rejection - db connection issue
+//listen to unhandledRejection
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Server Shutting down...');
+  console.log(err.name, err.message);
+  //shut down server
+  server.close(() => {
+    process.exit(1); //1 for uncaught exception
+  });
 });
